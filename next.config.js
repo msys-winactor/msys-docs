7c5d2c8980ae87a8e6f3ceb41e137e02c0dc8d59
d-shiina <daiki.shiina@gmail.com>
2025-05-29 18:58:24 +0900

    .gitignoreにout/を追加し、next.config.jsに画像の最適化設定を追加

--- next.config.js
+++ next.config.js
@@ -7,4 +7,7 @@ module.exports = withNextra({
   output: 'export',
   basePath: '/msys-docs', // GitHub Pages のサブパスに合わせる
   trailingSlash: true,
+  images: {
+    unoptimized: true, // ← これを追加
+  },
 })
