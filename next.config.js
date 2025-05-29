c1a1c44cf87ea0476964ad70cc7483b79add7983
d-shiina <daiki.shiina@gmail.com>
2025-05-29 22:08:13 +0900

    next.config.jsのassetPrefixを'/msys-docs/'に設定

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withNextra = require('nextra')({
 module.exports = withNextra({
   output: 'export',
   basePath: '/msys-docs', // GitHub Pages のサブパスに合わせる
+  assetPrefix: '/msys-docs/',
   trailingSlash: true,
   images: {
     unoptimized: true, // ← これを追加
